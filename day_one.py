f9772e4209599868139a33615e5e3d32d9df8087
jennizawlango <jennyzalwango12@gmail.com>
2018-11-07 15:25:03 +0300

    if user enter the year they were born there able to see their age

---
+++ day_one.py
@@ -0,0 +1,16 @@
+#in this l have used conditions,datatypes,inputs and output
+userage=int(input('enter the year you were born'))
+age = 2018-userage
+if age <0:
+   print('invalid age')
+elif age<18:
+   print('you are a minor')
+elif age<36:
+   print('you are a youth')
+elif age>37:
+   print('you are an elder')
+
+
+
+
+#for dictionaries
